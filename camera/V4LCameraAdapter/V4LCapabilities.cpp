5f0295a8cac99650c22617d0fcf2f2d7702b01bf
Saravanan Solaiyappan <saravanan.s@ti.com>
2012-11-01 00:35:24 -0500

    Revert "CameraHal: V4LCameraAdapter: Add support for MJPEG"
    This reverts commit c02cd8fb7a0b8c9f8bb1f065f5cb086c802a7088.
    
    Change-Id: I70433150f7f5384a28ba9c05ab2dec80d7398da5
    Signed-off-by: Saravanan Solaiyappan <saravanan.s@ti.com>

--- camera/V4LCameraAdapter/V4LCapabilities.cpp
+++ camera/V4LCameraAdapter/V4LCapabilities.cpp
@@ -262,7 +262,7 @@ status_t V4LCameraAdapter::getCaps(const int sensorId, CameraProperties::Propert
         frmSizeEnum.index = i;
         //Check for frame sizes for default pixel format
         //TODO: Check for frame sizes for all supported pixel formats
-        frmSizeEnum.pixel_format = DEFAULT_PIXEL_FORMAT;
+        frmSizeEnum.pixel_format = V4L2_PIX_FMT_YUYV;
         status = ioctl (handle, VIDIOC_ENUM_FRAMESIZES, &frmSizeEnum);
         if (status == NO_ERROR) {
             int width;
@@ -308,7 +308,7 @@ status_t V4LCameraAdapter::getCaps(const int sensorId, CameraProperties::Propert
         for ( i = 0; status == NO_ERROR; i++) {
             frmIvalEnum.index = i;
             //Check for supported frame rates for the default pixel format.
-            frmIvalEnum.pixel_format = DEFAULT_PIXEL_FORMAT;
+            frmIvalEnum.pixel_format = V4L2_PIX_FMT_YUYV;
             frmIvalEnum.width = caps.tPreviewRes[j].width;
             frmIvalEnum.height = caps.tPreviewRes[j].height;
 
